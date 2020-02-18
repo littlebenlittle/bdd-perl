1984cd5e4a9cf1d6039df438406959c5e8a42945
Ben Little <ben.little@fruition.net>
2020-02-18 13:31:02 -0700

    Clean up unit tests and give meaningful test name

--- tests/test_runner.pl
+++ tests/test_runner.pl
@@ -11,26 +11,36 @@ use BDD::Perl::Runner;
 my $runner = BDD::Perl::Runner->get_instance;
 
 is_deeply $runner, BDD::Perl::Runner->get_instance,
-  'Perl Runner is a singleton';
+'Perl Runner is a singleton';
 
-throws_ok { $runner->run('some arg') } qr/Runner has no registered definitions/,
-  'Cannot run anything on a runner with no registered definitions';
+throws_ok { $runner->run('') } qr/Runner has no registered definitions/,
+'Cannot run anything on a runner with no registered definitions';
 
 $runner->register(qr/SAYOK/, sub { 'OK'; });
-is $runner->run('SAYOK'), 'OK',
-  'Can run step';
+throws_ok { $runner->run('') } qr/No match found for .*/,
+'Cannot run an arg with no matching definition';
 
-ok not $runner->can_run('No matching definitions'),
-  'Step without matching definition cannot be run';
+is $runner->run('SAYOK'), 'OK',
+'Can run an arg with matching definition and get expected result';
+
+ok(not ($runner->can_run('No matching definitions')),
+'Step without matching definition cannot be run');
 
 $runner->register(qr/ECHO (.*)/, sub { $1; });
 is $runner->run('ECHO my argument'), 'my argument',
-   'Runner can return arg from regex match';
+'Runner can return arg from regex match';
 
+$runner->register(qr/TWO (.*) are some (.*)/, sub { $1 . $2; });
+is $runner->run('TWO here are some things'), 'herethings',
+'Runner can return TWO args from regex match';
+
+isnt($runner->{ctx}, undef,
+'Runner has a defined context');
+
+$runner->register(qr/CONTEXT/, sub { $runner->{ctx}->{somedata}; });
 $runner->{ctx}->{somedata} = 'my data';
-$runner->register(qr/CONTEXT/, sub { $_->{somedata}; });
 is $runner->run('CONTEXT'), 'my data',
-   'Step definitions can access runner\'s context';
+'Step definitions can access runner\'s context';
 
 done_testing();
 
